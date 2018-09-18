0a7279b10995c7f3050dedd006ea741040c71b40
seungdols <seungdols0822@gmail.com>
2018-09-18 23:10:51 +0900

    15.2 redux-promise-middleware 사용

--- src/modules/post.js
+++ src/modules/post.js
@@ -6,6 +6,7 @@ function getPostAPI (postId) {
     return axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
 }
 
+const GET_POST = 'GET_POST'
 const GET_POST_PENDING = 'GET_POST_PENDING'
 const GET_POST_SUCCESS = 'GET_POST_SUCCESS'
 const GET_POST_FAILURE = 'GET_POST_FAILURE'
@@ -15,18 +16,10 @@ const getPostSuccess = createAction(GET_POST_SUCCESS)
 const getPostFailure = createAction(GET_POST_FAILURE)
 
 //getPost thunk
-export const getPost = (postId) => dispatch => {
-    dispatch(getPostPending())
-
-    return getPostAPI(postId).then((response) => {
-        dispatch(getPostSuccess(response))
-
-        return response
-    }).catch(error => {
-        dispatch(getPostFailure(error))
-        throw(error)
-    })
-}
+export const getPost = (postId) => dispatch => ({
+    type: GET_POST,
+    payload: getPostAPI(postId)
+})
 
 const initialState = {
     pending: false,
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
-import modules from './modules';
+import { createStore, applyMiddleware } from 'redux'
+import modules from './modules'
 import { createLogger } from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
+import promiseMiddleware from 'redux-promise-middleware'
 
 const logger = createLogger()
-const store = createStore(modules, applyMiddleware(logger, ReduxThunk))
+const pm = promiseMiddleware({
+  promiseTypeSuffixes: ['PENDING', 'SUCCESS', 'FAILURE']
+})
+const store = createStore(modules, applyMiddleware(logger, ReduxThunk, pm))
 
-export default store;
+export default store
